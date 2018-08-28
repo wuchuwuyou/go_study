08f7326cd8997f47eb233e1614ac516a1c033830
Murphy <tinynorth@163.com>
2018-08-28 22:48:26 +0800

    init

---
+++ demo3.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"os"
+	"flag"
+	"fmt"
+)
+
+var name string
+var cmdLine = flag.NewFlagSet("question", flag.ExitOnError)
+
+func init()  {
+	flag.CommandLine = flag.NewFlagSet("", flag.ExitOnError)
+	// flag.CommandLine = flag.NewFlagSet("", flag.PanicOnError)
+
+	flag.CommandLine.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", "question")
+		flag.PrintDefaults()
+	}
+	flag.StringVar(&name,"name","everyone","The greeting object.")
+}
+
+func main() {
+	// flag.Usage = func() {
+	// 	fmt.Fprintf(os.Stderr, "Usage of %s:\n", "question")
+	// 	flag.PrintDefaults()
+	// }
+	// flag.Parse()
+	cmdLine.Parse(os.Args[1:])
+	fmt.Printf("Hello, %s!\n", name)
+
+}
